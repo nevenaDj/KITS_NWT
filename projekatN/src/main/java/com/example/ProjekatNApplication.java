5479f446f089d0a2cf2e5df4c17e65383556d164
Nevena Đerić <nevena5695@gmail.com>
2017-10-18 16:09:19 +0200

    initial commit

---
+++ projekatN/src/main/java/com/example/ProjekatNApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatNApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekatNApplication.class, args);
+	}
+}
