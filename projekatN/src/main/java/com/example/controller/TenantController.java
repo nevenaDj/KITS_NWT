3295754cc17f767a4811e1bd0350ad99f24092ca
Nevena Đerić <nevena5695@gmail.com>
2017-11-03 22:12:28 +0100

    create, delete, update, see all tenants (#1-9, #1-10, #3-4, #1-16)

---
+++ projekatN/src/main/java/com/example/controller/TenantController.java
@@ -0,0 +1,81 @@
+package com.example.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dto.UserDTO;
+import com.example.model.User;
+import com.example.service.UserService;
+
+@RestController
+@RequestMapping(value = "api/tenants")
+public class TenantController {
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	@Autowired
+	UserService userService;
+
+	@RequestMapping(method = RequestMethod.POST, consumes = "application/json")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public ResponseEntity<UserDTO> addTenant(@RequestBody UserDTO userDTO) {
+		User user = UserDTO.getUser(userDTO);
+		user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+
+		user = userService.save(user, "ROLE_USER");
+		return new ResponseEntity<UserDTO>(new UserDTO(user), HttpStatus.CREATED);
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<UserDTO>> getTenants(Pageable page) {
+		Page<User> users = userService.find(page, "ROLE_USER");
+
+		List<UserDTO> usersDTO = new ArrayList<UserDTO>();
+		for (User user : users) {
+			usersDTO.add(new UserDTO(user));
+		}
+		return new ResponseEntity<List<UserDTO>>(usersDTO, HttpStatus.OK);
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, consumes = "application/json")
+	public ResponseEntity<UserDTO> updateTenant(@RequestBody UserDTO userDTO) {
+		User user = userService.findOne(userDTO.getId());
+
+		if (user == null) {
+			return new ResponseEntity<UserDTO>(HttpStatus.BAD_REQUEST);
+		}
+
+		user.setEmail(userDTO.getEmail());
+		user.setAddress(userDTO.getAddress());
+		user.setPhoneNo(userDTO.getPhoneNo());
+
+		user = userService.update(user);
+
+		return new ResponseEntity<UserDTO>(new UserDTO(user), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public ResponseEntity<Void> deleteTenant(@PathVariable Long id) {
+		User user = userService.findOne(id);
+		if (user == null) {
+			return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+		} else {
+			userService.remove(id, "ROLE_USER");
+			return new ResponseEntity<Void>(HttpStatus.OK);
+		}
+	}
+
+}
---
+++ projekatN/src/main/java/com/example/service/UserService.java
@@ -0,0 +1,59 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.example.model.Authority;
+import com.example.model.User;
+import com.example.model.UserAuthority;
+import com.example.repository.AuthorityRepository;
+import com.example.repository.UserAuthorityRepository;
+import com.example.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	UserRepository userRepository;
+	@Autowired
+	UserAuthorityRepository userAuthorityRepository;
+	@Autowired
+	AuthorityRepository authorityRepository;
+
+	public User save(User user, String role) {
+		user = userRepository.save(user);
+		Authority authority = authorityRepository.findByName(role);
+
+		UserAuthority userAuthority = new UserAuthority(user, authority);
+		userAuthority = userAuthorityRepository.save(userAuthority);
+
+		return user;
+	}
+
+	public User update(User user) {
+		return userRepository.save(user);
+	}
+
+	public Page<User> find(Pageable page, String role) {
+		return userRepository.find(role, page);
+	}
+
+	public Page<User> findAll(Pageable page) {
+		return userRepository.findAll(page);
+	}
+
+	public User findOne(Long id) {
+		return userRepository.findOne(id);
+	}
+
+	public void remove(Long id, String role) {
+		Authority authority = authorityRepository.findByName(role);
+		Long userAuthorityID = userAuthorityRepository.findByUserAndAuthority(id, authority.getId());
+		userAuthorityRepository.delete(userAuthorityID);
+		userRepository.delete(id);
+
+	}
+
+}
