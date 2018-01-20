b3993924bbdb488e123373aaf511d58aec672d17
Nevena Đerić <nevena5695@gmail.com>
2018-01-20 12:45:18 +0100

    css

--- frontend/src/main/frontend/src/app/home/home.component.ts
+++ frontend/src/main/frontend/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import * as decode from 'jwt-decode';
 
 import { AuthService } from '../login/auth.service';
 
