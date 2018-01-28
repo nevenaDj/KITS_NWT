db1aef3da0db10407ed198e3be7d64c6e5ddfd8c
Nevena Đerić <nevena5695@gmail.com>
2018-01-28 23:10:18 +0100

    unit test notification

--- projekatN/src/test/java/com/example/controller/NotificationControllerTest.java
+++ projekatN/src/test/java/com/example/controller/NotificationControllerTest.java
@@ -124,7 +124,7 @@ public class NotificationControllerTest {
 	@Test
 	public void testGetNotification() throws Exception {
 		mockMvc.perform(
-				get("/api/buildings/" + ID_BUILDING + "/notifications/" + ID).header("X-Auth-Token", accessToken))
+				get("/api/notifications/" + ID).header("X-Auth-Token", accessToken))
 				.andExpect(status().isOk()).andExpect(jsonPath("$.id").value(ID.intValue()))
 				.andExpect(jsonPath("$.text").value(TEXT));
 
@@ -135,19 +135,11 @@ public class NotificationControllerTest {
 	@Rollback(true)
 	public void testDeleteNotification() throws Exception {
 		mockMvc.perform(
-				delete("/api/buildings/" + ID_BUILDING + "/notifications/" + ID).header("X-Auth-Token", accessToken))
+				delete("/api/notifications/" + ID).header("X-Auth-Token", accessToken))
 				.andExpect(status().isOk());
 
 	}
 
-	@Test
-	@Transactional
-	@Rollback(true)
-	public void testDeleteNotificationBadRequest() throws Exception {
-		mockMvc.perform(delete("/api/buildings/" + BUILDING_ID_NOT_FOUND + "/notifications/" + ID)
-				.header("X-Auth-Token", accessToken)).andExpect(status().isBadRequest());
-
-	}
 
 	@Test
 	@Transactional
@@ -163,13 +155,6 @@ public class NotificationControllerTest {
 		mockMvc.perform(
 				get("/api/buildings/" + ID_BUILDING + "/notifications/" + ID_NEW).header("X-Auth-Token", accessToken))
 				.andExpect(status().isNotFound());
-
-	}
-
-	@Test
-	public void testGetNotificationBadRequest() throws Exception {
-		mockMvc.perform(get("/api/buildings/" + BUILDING_ID_NOT_FOUND + "/notifications/" + ID).header("X-Auth-Token",
-				accessToken)).andExpect(status().isBadRequest());
 
 	}
 
